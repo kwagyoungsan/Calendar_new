096164f9d9ceb24cc52a65bdc65b42fb22ee4e88
youngsan <super9609@naver.com>
2021-09-20 21:25:22 +0900

    -

--- app/src/main/java/com/example/calendar/MainActivity.kt
+++ app/src/main/java/com/example/calendar/MainActivity.kt
@@ -3,17 +3,18 @@ package com.example.calendar
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.ActionBar
 import com.example.calendar.databinding.ActivityMainBinding
 import com.prolificinteractive.materialcalendarview.*
 import java.util.Calendar
+import android.util.Log
+import android.widget.FrameLayout
 
 
 class MainActivity : AppCompatActivity() {
     private var mBinding: ActivityMainBinding? = null
     private val binding get() = mBinding!!
-
-    val manager = supportFragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +30,8 @@ class MainActivity : AppCompatActivity() {
         val currentDate = startTimeCalendar.get(Calendar.DATE)
 
         val materialCalendar: MaterialCalendarView = findViewById(R.id.materialCalendar)
+
+        val VIEW1: FrameLayout = findViewById(R.id.view)
 
         actionBar = supportActionBar
         actionBar?.hide()
@@ -69,40 +72,49 @@ class MainActivity : AppCompatActivity() {
             todayDecorator
         )
 
-        fun ShowTabOne() {
-            val transaction = manager.beginTransaction()
-            val fragment = MenuFragment()
-            transaction.replace(R.id.menuFragment, fragment)
-            transaction.addToBackStack(null)
+        binding.menubt.setOnClickListener {
+            val TAG:String = "MainActivity : "
+            Log.e(TAG, "Log ----- MainActivity")
+
+            val bundle = Bundle()
+            Log.e(TAG, "Log ----- bundle : "+bundle)
+            bundle.putString("Key", "Hello MenuFragment")
+
+
+            val menuFragment = MenuFragment()
+            menuFragment.arguments = bundle
+            val transaction = supportFragmentManager.beginTransaction()
+            Log.e(TAG, "Log ----- transaction : "+transaction)
+            transaction.replace(R.id.view, menuFragment)
+            Log.e(TAG, "Log ----- transaction : "+transaction)
             transaction.commit()
+            Log.e(TAG, "Log ----- transaction : "+transaction)
+//            binding.materialCalendar.visibility = View.INVISIBLE
+//            binding.a1.visibility = View.INVISIBLE
+//            binding.a2.visibility = View.INVISIBLE
+
         }
 
+        binding.calendarbt.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
-        binding.menubt.setOnClickListener {
-            val bundle = Bundle()
-            bundle.putString("Key", "Hello FragmentA")
-            ShowTabOne()
+        binding.habitbt.setOnClickListener {
+            val intent = Intent(this, HabitActivity::class.java)
+            startActivity(intent)
+        }
 
-
-            binding.calendarbt.setOnClickListener {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-            }
-
-            binding.habitbt.setOnClickListener {
-                val intent = Intent(this, HabitActivity::class.java)
-                startActivity(intent)
-            }
-
-            binding.settingbt.setOnClickListener {
-                val intent = Intent(this, SettingActivity::class.java)
-                startActivity(intent)
-            }
+        binding.settingbt.setOnClickListener {
+            val intent = Intent(this, SettingActivity::class.java)
+            startActivity(intent)
         }
     }
 
+
     override fun onDestroy() {
         mBinding = null
+
         super.onDestroy()
     }
 }
--- app/src/main/java/com/example/calendar/MenuFragment.kt
+++ app/src/main/java/com/example/calendar/MenuFragment.kt
@@ -1,17 +1,19 @@
 package com.example.calendar
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import com.example.calendar.databinding.FragmentMenuBinding
 
 
 class MenuFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var _binding : FragmentMenuBinding? = null
-    private val binding get() = _binding!!
+//    private var _binding: FragmentMenuBinding? = null
+//    private val binding get() = _binding!!
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,10 +27,13 @@ class MenuFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentMenuBinding.inflate(inflater, container, false)
+        val TAG:String = "MenuFragment : "
+        Log.e(TAG, "Log ----- MenuFragment")
+        val binding = FragmentMenuBinding.inflate(inflater, container, false)
+        Log.e(TAG, "Log ----- binding : "+binding)
         binding.textView.text = arguments?.getString("Key")
+        Log.e(TAG, "Log ----- key : "+binding.textView.text)
         return binding.root
-
 
     }
 
