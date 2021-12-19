275112cc3d09fc35eeefba5ca4982c64cfd9cb14
youngsan <super9609@naver.com>
2021-12-19 19:17:08 +0900

    - GlobalApplication.kt

--- app/src/main/java/com/example/calendar/App.kt
+++ app/src/main/java/com/example/calendar/App.kt
@@ -1,20 +1,14 @@
 package com.example.calendar
 
 import android.app.Application
-import com.kakao.sdk.auth.AuthApiClient.Companion.instance
-import com.kakao.sdk.common.KakaoSdk
 
-class GlobalApplication : Application() {
+class App : Application() {
     companion object {
         lateinit var prefs : MySharedPreferences
     }
 
     override fun onCreate() {
         prefs = MySharedPreferences(applicationContext)
-        KakaoSdk.init(this, "7d93b360b9e054deeba80bd62b95ec25")
-
         super.onCreate()
     }
-
-
 }
