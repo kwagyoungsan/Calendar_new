fb739f7e2c0fc931bc85040f94506365bd535433
kwagyoungsan <61777583+kwagyoungsan@users.noreply.github.com>
2021-12-30 17:36:06 +0900

    - Save data in array list using sharedpreference.

--- app/src/main/java/com/example/calendar/GlobalApplication.kt
+++ app/src/main/java/com/example/calendar/GlobalApplication.kt
@@ -5,11 +5,11 @@ import com.kakao.sdk.auth.AuthApiClient.Companion.instance
 import com.kakao.sdk.common.KakaoSdk
 
 class GlobalApplication : Application() {
-    companion object { lateinit var prefs: PreferenceUtil }
+
 
 
     override fun onCreate() {
-        prefs = PreferenceUtil(applicationContext)
+
 
         KakaoSdk.init(this, "7d93b360b9e054deeba80bd62b95ec25")
 
